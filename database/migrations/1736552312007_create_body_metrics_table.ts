5b59f6de13c287419cfe342de014586bdcdccd7b
Guilherme Ytalo <gydaytalo@gmail.com>
2025-01-10 21:54:20 -0300

    feat: created DB structure with ORM and validate relationships and type check

--- database/migrations/1736552312007_create_body_metrics_table.ts
+++ database/migrations/1736552312007_create_body_metrics_table.ts
@@ -6,7 +6,13 @@ export default class extends BaseSchema {
   async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-
+      table.integer('user_id').unsigned().references('users.id')
+      table.date('record_date').notNullable()
+      table.decimal('current_weight').notNullable()
+      table.decimal('target_weight').notNullable()
+      table.decimal('chest_size').notNullable()
+      table.decimal('waist_size').notNullable()
+      table.decimal('hip_size').notNullable()
       table.timestamp('created_at')
       table.timestamp('updated_at')
     })
